c09f12e7b7cce113eb4cdfc7f8d50a4aec3d7c7e
Angelica <Angelica137@users.noreply.github.com>
2020-09-19 13:26:05 +0100

    get data from api

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -20,7 +20,25 @@ class Profile extends Component {
     }
   }
   render() {
-    return <div></div>;
+    const { data, loading } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <ul>
+          <li>avatar_url: {data.avatar_url}</li>
+          <li>avatar_url: {data.html_url}</li>
+          <li>avatar_url: {data.repos_url}</li>
+          <li>avatar_url: {data.name}</li>
+          <li>avatar_url: {data.company}</li>
+          <li>avatar_url: {data.location}</li>
+          <li>avatar_url: {data.email}</li>
+          <li>avatar_url: {data.bio}</li>
+        </ul>
+      </div>
+    );
   }
 }
 
