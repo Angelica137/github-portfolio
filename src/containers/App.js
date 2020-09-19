ee3b9e63081af2a7dd0b3e27ac665428a7fd679e
Angelica <Angelica137@users.noreply.github.com>
2020-09-19 22:36:29 +0100

    add styled components to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 import Profile from "./Profile";
 import Header from "../components/Header/Header";
 
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header logo={logo} />
+      <AppWrapper>
+        <Header />
         <Profile />
-      </div>
+      </AppWrapper>
     );
   }
 }
