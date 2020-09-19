ff4f88287b2f36d688d10c2fc1d2ead855bccd5f
Angelica <Angelica137@users.noreply.github.com>
2020-09-19 11:57:47 +0100

    amend path to header component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Header from "../components/App/Header";
+import Header from "../components/Header/Header";
 import logo from "../logo.svg";
 import "./App.css";
 
