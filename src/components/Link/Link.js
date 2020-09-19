e24bc052162a4a171340c8cd7a6c02445a57fce7
Angelica <Angelica137@users.noreply.github.com>
2020-09-19 12:17:42 +0100

    refactor link component to use props

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,14 +1,9 @@
 import React from "react";
 import "./Link.css";
 
-const Link = () => (
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
+const Link = ({ url, title }) => (
+  <a className="App-link" href={url} target="_blank" rel="noopener noreferrer">
+    {title}
   </a>
 );
 
