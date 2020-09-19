7d921ad98e8c16baae18bbb5c19c462bcf10c119
Angelica <Angelica137@users.noreply.github.com>
2020-09-19 13:54:14 +0100

    refactor profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Link from "../components/Link/Link";
+import "./Profile.css";
 
 class Profile extends Component {
   constructor() {
@@ -26,16 +28,37 @@ class Profile extends Component {
       return <div>Loading...</div>;
     }
     return (
-      <div>
+      <div className="Profile-container">
+        <img className="Profile-avatar" src={data.avatar_url} alt="avatar" />
         <ul>
-          <li>avatar_url: {data.avatar_url}</li>
-          <li>avatar_url: {data.html_url}</li>
-          <li>avatar_url: {data.repos_url}</li>
-          <li>avatar_url: {data.name}</li>
-          <li>avatar_url: {data.company}</li>
-          <li>avatar_url: {data.location}</li>
-          <li>avatar_url: {data.email}</li>
-          <li>avatar_url: {data.bio}</li>
+          <li>
+            <strong>html_url:</strong>
+            <Link url={data.html_url} title="Github URL" />
+          </li>
+          <li>
+            <strong>repos_url:</strong>
+            {data.repos_url}
+          </li>
+          <li>
+            <strong>name:</strong>
+            {data.name}
+          </li>
+          <li>
+            <strong>company:</strong>
+            {data.company}
+          </li>
+          <li>
+            <strong>location:</strong>
+            {data.location}
+          </li>
+          <li>
+            <strong>email:</strong>
+            url={data.html_url}
+          </li>
+          <li>
+            <strong>bio:</strong>
+            url={data.bio}
+          </li>
         </ul>
       </div>
     );
