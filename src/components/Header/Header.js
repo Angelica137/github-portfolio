746e2db5c25ad8a4eabe9d83963cb54e848d8ad2
Angelica <Angelica137@users.noreply.github.com>
2020-09-19 14:26:12 +0100

    ammend header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,11 @@
 import React from "react";
-import logo from "../../GitHub-Mark-Light-64px.png";
+import logo from "../../logo.svg";
 import "./Header.css";
 
 const Header = () => (
-  <header className="App-header">
-    <header className="App-Header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-    <h1>
-      Edit <code>src/App.js</code>and save to reload.
-    </h1>
+  <header className="App-Header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>My Github portfolio</h1>
   </header>
 );
 
